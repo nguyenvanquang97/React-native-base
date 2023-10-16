071451f4957afe95495c8c16fabbedfbcf3f0eee
dvhai03 <131355092+dvhai03@users.noreply.github.com>
2023-10-16 14:10:24 +0700

    abc

---
+++ App.tsx
@@ -0,0 +1,28 @@
+import React, { Component } from 'react';
+//Redux
+import { legacy_createStore as createStore, applyMiddleware } from 'redux';
+import { Provider } from 'react-redux';
+import allReducers from './redux/reducers';
+
+//Redux saga
+import createSagaMiddleware from 'redux-saga';
+import rootSaga from './redux/sagas/rootSaga';
+
+import ListSon from './redux/components/ListSon';
+import RegistrationForm from './redux/components/RegistrationForm';
+//Middleware
+const sagaMiddleware = createSagaMiddleware();
+//Từ applyMiddleware vào Reducers thì tạo một store, sagaMiddleware nằm giữa Action và Reducers.
+let store = createStore(allReducers, applyMiddleware(sagaMiddleware));
+const App = () => (
+  <Provider store={store}>
+    <RegistrationForm />
+  </Provider>
+);
+export default class AppSaga extends Component {
+  render() {
+    return <App />;
+  }
+}
+
+sagaMiddleware.run(rootSaga); //Chạy xuyên suốt các hàm rootSaga trong app 
